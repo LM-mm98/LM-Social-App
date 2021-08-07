c9e3a57c51cf26406b38049de27bc0f501d452bf
Lin Myat <linmt55@gmail.com>
2021-08-07 12:05:13 +0630

    First Commit

---
+++ Social App/Controller/SignInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social App
+//
+//  Created by Lin Myat on 07/08/2021.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
