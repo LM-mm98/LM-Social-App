351009f344454f0449b6f97ed2fca8b15dc50e30
LM-mm98 <77261637+LM-mm98@users.noreply.github.com>
2021-08-24 16:24:36 +0630

    Firebase get complete

--- Social App/Controller/FeedVC.swift
+++ Social App/Controller/FeedVC.swift
@@ -10,11 +10,15 @@ import SwiftKeychainWrapper
 import Firebase
 
 class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("\(post.caption)")
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as? FeedTableViewCell else{
             return UITableViewCell() }
@@ -23,9 +27,11 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
     @IBOutlet weak var tableView: UITableView!
-    
 
     @IBOutlet weak var feedTableView: UITableView!
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,7 +40,18 @@ class FeedVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         tableView.dataSource = self
         
         DataService.dataService.reference_Posts.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+//            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, Any> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.feedTableView.reloadData()
         })
     }
     
